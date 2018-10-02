5b02556fffdb5abbcdf98e125ebbf1a9b5879a72
Joana Marques <joana.pipaa@gmail.com>
2018-10-02 11:05:59 +0100

    Add some code

--- MyAula01/MyFortnite/FNPlayer.cs
+++ MyAula01/MyFortnite/FNPlayer.cs
@@ -3,8 +3,34 @@ namespace MyFortnite
 {
     public class FNPlayer
     {
-        public FNPlayer()
+ 		string equipedWeapon;
+        double hitPoints;
+        double shield;
+
+        public void Attack(FNPlayer enemy)
         {
+            enemy.TakeDamage(10);
         }
+
+        public void TakeDamage(double damage)
+        {
+            if (shield > 0)
+            {
+                shield -= damage;
+                if (shield < 0)
+                {
+                    shield = 0;
+                }
+            } else 
+            {
+                hitPoints -= damage;
+                if (hitPoints <= 0)
+                {
+                    Die();
+                }
+            }
+        }
+
     }
 }
+
