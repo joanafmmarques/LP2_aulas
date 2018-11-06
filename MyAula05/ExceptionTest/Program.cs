8958222558cd8e3756d550c8eedfa77dd18a684a
Joana Marques <joana.pipaa@gmail.com>
2018-11-06 10:40:09 +0000

    Add finally

--- MyAula05/ExceptionTest/Program.cs
+++ MyAula05/ExceptionTest/Program.cs
@@ -24,6 +24,10 @@ namespace ExceptionTest
             {
                 Console.WriteLine("Ocorreu um problema.");
             }
+            finally
+            {
+                Console.WriteLine("Obrigado por ter utilizado este programa!");
+            }
 
         }
     }
