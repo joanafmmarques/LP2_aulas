32b0c048f7af5c64e1c68f96d4ae64b6584ab4fd
Joana Marques <joana.pipaa@gmail.com>
2018-11-06 10:19:56 +0000

    Exception

--- MyAula05/ExceptionTest/Program.cs
+++ MyAula05/ExceptionTest/Program.cs
@@ -7,8 +7,16 @@ namespace ExceptionTest
         static void Main(string[] args)
         {
             Console.Write("Insere um numero inteiro: ");
-            int i = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Numero inserido: {i}");
+            try 
+            { 
+                int i = Convert.ToInt32(Console.ReadLine()); 
+                Console.WriteLine($"Numero inserido: {i}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ocorreu o seguinte problema: " + e.Message);
+            }
+
         }
     }
 }
